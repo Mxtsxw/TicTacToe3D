2dffb1419a1f3dfe3edada7cdd4b8db97a23a414
Matsew-uwu <randriantsoamatthieu@gmail.com>
2023-01-30 01:10:52 +0100

    Ajouts des méthodes génériques dans la classe Morpion

--- TicTacToeApp/src/main/java/Morpions/Morpion.java
+++ TicTacToeApp/src/main/java/Morpions/Morpion.java
@@ -2,4 +2,15 @@ package Morpions;
 
 public class Morpion {
 
+    public void init() {}
+
+    public void afficher() {}
+
+    public void afficherEmplacement(int index) {}
+
+    public void placer(int index, String value) {}
+
+    public boolean checkWin() {
+        return false;
+    }
 }
