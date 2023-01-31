39fb1d65e0a18ea10f9d252d31232bbdace16300
Matsew-uwu <randriantsoamatthieu@gmail.com>
2023-01-31 14:29:44 +0100

    Executable lancement du jeu

--- TicTacToeApp/src/main/java/Executable.java
+++ TicTacToeApp/src/main/java/Executable.java
@@ -2,21 +2,7 @@ import Morpions.*;
 
 public class Executable {
     public static void main(String[] args) {
-        System.out.println("Lancement du programme");
-
-        Morpion m2d = new Morpion2D();
-        System.out.println(m2d);
-        m2d.init();
-        System.out.println(m2d);
-        m2d.afficher();
-        m2d.afficherEmplacement(1);
-        m2d.placer(1, "X");
-        m2d.afficher();
-        System.out.println(m2d.checkWin());
-        m2d.placer(2, "X");
-        System.out.println(m2d.checkWin());
-        m2d.placer(3, "X");
-        System.out.println(m2d.checkWin());
+        System.out.println("Lancement du jeu");
 
         TicTacToe Game = new TicTacToe();
         Game.jouer();
